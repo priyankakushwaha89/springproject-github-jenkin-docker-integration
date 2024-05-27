23796ebe2396aba645b0108cb2d71e37f7f56702
priyanka <priyankakushwaha7762@gmail.com>
2024-05-27 13:29:28 +0530

    first commit

---
+++ src/main/java/com/integration/docker_jenkins_integration_sample/DockerJenkinsIntegrationSampleApplication.java
@@ -0,0 +1,13 @@
+package com.integration.docker_jenkins_integration_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerJenkinsIntegrationSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerJenkinsIntegrationSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/integration/docker_jenkins_integration_sample/DockerJenkinsIntegrationSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.integration.docker_jenkins_integration_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerJenkinsIntegrationSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
