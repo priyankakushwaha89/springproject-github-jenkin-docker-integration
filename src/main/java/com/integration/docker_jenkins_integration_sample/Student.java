4863c4e88e6d0affbda4f6e6d75ab8c47c6422ec
priyanka <priyankakushwaha7762@gmail.com>
2024-05-27 14:13:19 +0530

    Second commit

---
+++ src/main/java/com/integration/docker_jenkins_integration_sample/Student.java
@@ -0,0 +1,37 @@
+package com.integration.docker_jenkins_integration_sample;
+
+import jakarta.persistence.Entity;
+
+@Entity(name="student")
+public class Student 
+{
+ int id;
+ String name;
+ String city;
+ int age;
+public int getId() {
+	return id;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getCity() {
+	return city;
+}
+public void setCity(String city) {
+	this.city = city;
+}
+public int getAge() {
+	return age;
+}
+public void setAge(int age) {
+	this.age = age;
+}
+ 
+}
